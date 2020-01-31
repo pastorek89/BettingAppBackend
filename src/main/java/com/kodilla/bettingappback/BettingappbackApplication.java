737b97d8c1679ebfb51e4c6724b2a0bc5026ed2e
pastorek89 <pastor.patryk@gmail.com>
2020-01-31 08:32:31 +0100

    First Commit

---
+++ src/main/java/com/kodilla/bettingappback/BettingappbackApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.bettingappback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BettingappbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BettingappbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/bettingappback/BettingappbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.bettingappback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BettingappbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
